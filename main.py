2222acce8445b490f7e5879daf3b773f0a074f3f
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-12-31 20:07:14 -0500

    🚀 update main.py

--- main.py
+++ main.py
@@ -21,22 +21,20 @@ browser = webdriver.Chrome(
 
 browser.get("https://eltiempo.es")
 
-button_class = ".didomi-components-button didomi-button didomi-dismiss-button didomi-components-button--color didomi-button-highlight highlight-button".replace(" ", ".")
 
+button_class = " didomi-components-button didomi-button didomi-dismiss-button didomi-components-button--color didomi-button-highlight highlight-button".replace(" ", ".")
 WebDriverWait(browser, 5)\
     .until(EC.element_to_be_clickable((By.CSS_SELECTOR, button_class)))\
     .click()
 
-search_input_class = "input#term.text-roboto-condensed"
 
+search_input_class = "input#term.text-roboto-condensed"
 WebDriverWait(browser, 5)\
     .until(EC.element_to_be_clickable((By.CSS_SELECTOR, search_input_class)))\
     .send_keys("Madrid")
 
 
-search_icon_class = ".icon.icon-sm.icon-search"
-
-
+search_icon_class = "i.icon.icon-sm.icon-search"
 WebDriverWait(browser, 5)\
     .until(EC.element_to_be_clickable((By.CSS_SELECTOR, search_icon_class)))\
     .click()
@@ -47,10 +45,38 @@ WebDriverWait(browser, 5)\
     .until(EC.element_to_be_clickable((By.CSS_SELECTOR, item_li_class)))\
     .click()
 
-xpath_link = "/html/body/div[5]/div[1]/div[4]/div/section[3]/section/div[1]/section/ul[1]/li[2]/h2/a"
+
+xpath_link = '/html/body/div[5]/div[1]/div[4]/div/main/section[3]/section/div/article/section/ul[1]/li[2]/h2/a'
 WebDriverWait(browser, 5)\
     .until(EC.element_to_be_clickable((By.XPATH, xpath_link)))\
     .click()
 
 
-time.sleep(5)
+xpath_ul ='//*[@id="cityTable"]/div[1]/ul/li[1]/ul'
+WebDriverWait(browser, 5)\
+    .until(EC.element_to_be_clickable((By.XPATH, xpath_ul)))\
+
+
+cols = browser.find_element("xpath", xpath_ul)
+cols = cols.text
+today = cols.split("\n")
+today = today[1:]
+# print("today: ", today)
+
+data = {
+    "hours": [],
+    "temperature": [],
+    "wind_speed": [] 
+}
+
+for i in range(0, len(today), 7):
+    data["hours"].append(today[i])
+    data["temperature"].append(today[i + 2])
+    data["wind_speed"].append(today[i + 5])
+
+
+df = pd.DataFrame(data)
+df.to_csv("data.csv", index=False)
+
+
+browser.quit()
